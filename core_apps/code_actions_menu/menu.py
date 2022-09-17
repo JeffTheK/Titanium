250d65bb04a9e1f6b0840896a032b8a1bc44e301
JeffTheK <jeff144@protonmail.com>
2022-09-17 15:48:11 +0300

    add code_runner lib

--- core_apps/code_actions_menu/menu.py
+++ core_apps/code_actions_menu/menu.py
@@ -2,6 +2,7 @@ import tkinter.ttk as ttk
 import tkinter as tk
 import pygubu
 import sys
+from .. import code_runner
 
 class CodeActionsWidget:
     def __init__(self, master, edit_area_text):
@@ -12,24 +13,11 @@ class CodeActionsWidget:
         self.edit_area_text.bind("<Button-3>", self.popup_right_click_menu) # Button-2 on Aqua
 
     def on_do_it_pressed(self):
-        self.execute_code()
-
-    def execute_code(self):
-        original_stdout_write = sys.stdout.write
-        original_stderr_write = sys.stderr.write
-        sys.stdout.write = self.stdout_write_redirector
-        sys.stderr.write = self.stdout_write_redirector
-
         code = self.edit_area_text.get(tk.SEL_FIRST, tk.SEL_LAST)
-        output = exec(code)
+        output, out_text = code_runner.run_code(code, "python")
+        self.edit_area_text.insert("end", out_text)
         if output != None:
             self.edit_area_text.insert("end", str(output))
-
-        sys.stdout.write = original_stdout_write
-        sys.stderr.write = original_stderr_write
-
-    def stdout_write_redirector(self, input_str):
-        self.edit_area_text.insert("end", input_str)
 
     def popup_right_click_menu(self, event):
         try:
---
+++ core_apps/code_runner/__init__.py
@@ -0,0 +1,29 @@
+import sys
+
+SUPPORTED_LANGUAGES = ["python"]
+OUT_TEXT = ""
+
+def run_code(code, language):
+    if language == "python":
+        return run_python_code(code)
+    else:
+        raise Exception
+
+def run_python_code(code):
+    global OUT_TEXT
+    OUT_TEXT = ""
+    original_stdout_write = sys.stdout.write
+    original_stderr_write = sys.stderr.write
+    sys.stdout.write = stdout_write_redirector
+    sys.stderr.write = stdout_write_redirector
+
+    output = exec(code)
+
+    sys.stdout.write = original_stdout_write
+    sys.stderr.write = original_stderr_write
+
+    return (output, OUT_TEXT)
+
+def stdout_write_redirector(input_str):
+    global OUT_TEXT
+    OUT_TEXT += input_str

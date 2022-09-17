f0357a29d6b2644d0ed87dfa956fca176ed9d50a
JeffTheK <jeff144@protonmail.com>
2022-09-17 16:54:01 +0300

    desktop redesign menu bar code

--- core_apps/desktop/src/main.py
+++ core_apps/desktop/src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import pathlib
 import tkinter.ttk as ttk
+import tkinter as tk
 import pygubu
 import os
 import json
@@ -8,6 +9,8 @@ import json
 PROJECT_PATH = pathlib.Path(__file__).parent
 PROJECT_UI = PROJECT_PATH / "main.ui"
 AUTO_START_APPS = ["transcript", "tutorial"]
+CODE_TOOLS = ["transcript", "project_explorer"]
+HELP_APPS = ["tutorial"]
 
 class MainApp:
     def __init__(self, master=None):
@@ -16,6 +19,26 @@ class MainApp:
         builder.add_from_file(PROJECT_UI)
         self.mainwindow = builder.get_object("toplevel1", master)
         builder.connect_callbacks(self)
+        self.setup_menu()
+
+    def setup_menu(self):
+        self.menubar = tk.Menu(self.mainwindow)
+
+        code_tools = tk.Menu(self.menubar, tearoff=0)
+        for code_tool in CODE_TOOLS:
+            name = code_tool.replace("_", " ")
+            name = name.title()
+            code_tools.add_command(label=name, command=lambda: self.run_project(f"core_apps/{code_tool}"))
+        self.menubar.add_cascade(label="Code Tools", menu=code_tools)
+
+        help_apps = tk.Menu(self.menubar, tearoff=0)
+        for app in HELP_APPS:
+            name = app.replace("_", " ")
+            name = name.title()
+            help_apps.add_command(label=name, command=lambda: self.run_project(f"core_apps/{app}"))
+        self.menubar.add_cascade(label="Help", menu=help_apps)
+
+        self.mainwindow.config(menu=self.menubar)
 
     def run(self):
         for app in AUTO_START_APPS:
@@ -27,12 +50,6 @@ class MainApp:
         project_json = json.loads(project_file.read())
         run_command = project_json["run_command"]
         os.system(run_command)
-
-    def on_open_transcript_button_clicked(self):
-        self.run_project("core_apps/transcript")
-
-    def on_open_project_explorer_button_clicked(self):
-        self.run_project("core_apps/project_explorer")
 
 if __name__ == "__main__":
     app = MainApp()

e13bcea28743895908e966ea5b1ba67a3e670378
JeffTheK <jeff144@protonmail.com>
2022-09-18 18:18:03 +0300

    code actions add language setting

--- core_apps/code_actions_menu/menu.py
+++ core_apps/code_actions_menu/menu.py
@@ -9,6 +9,7 @@ from .. import irontk
 class CodeActionsWidget:
     def __init__(self, master, edit_area_text):
         master.code_actions_widget = self
+        self.selected_language = "python"
         self.edit_area_text = edit_area_text
         irontk.popup_menu.setup(self.edit_area_text)
         self.do_it_image = tk.PhotoImage(file=Path( __file__ ).parent / "do_it.png")
@@ -16,7 +17,7 @@ class CodeActionsWidget:
 
     def on_do_it_pressed(self):
         code = self.edit_area_text.get(tk.SEL_FIRST, tk.SEL_LAST)
-        output, out_text = code_runner.run_code(code, "python")
+        output, out_text = code_runner.run_code(code, self.selected_language)
         self.edit_area_text.insert("end", out_text)
         if output != None:
             self.edit_area_text.insert("end", str(output))

473925d7868a1e2119b589ded7d86f8341a23aea
JeffTheK <jeff144@protonmail.com>
2022-09-21 13:34:51 +0300

    irontk popup menu add cursor hand

--- core_apps/irontk/popup_menu.py
+++ core_apps/irontk/popup_menu.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 class PopupMenu(tk.Menu):
     def __init__(self, master):
-        tk.Menu.__init__(self, master, tearoff=0)
+        tk.Menu.__init__(self, master, tearoff=0, cursor="hand2")
         master.bind("<Button-3>", lambda event: popup_right_click_menu(master, event)) # Button-2 on Aqua
 
 def popup_right_click_menu(master, event):

a852bd4c134994381736c9f893d9c1f0ec0c3f96
JeffTheK <jeff144@protonmail.com>
2022-09-21 13:47:13 +0300

    irontk add example app

---
+++ core_apps/irontk/example_app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import tkinter as tk
+import tkinter.ttk as ttk
+from .popup_menu import PopupMenu
+
+class ExampleApp:
+    def __init__(self, master=None):
+        self.frame1 = ttk.Frame(master)
+
+        # build ui
+        self.label1 = ttk.Label(master, text="right click me!")
+        self.label1.popupmenu = PopupMenu(self.label1)
+        self.label1.popupmenu.add_command(label="Option1", command=lambda: print("Option1"))
+        self.label1.popupmenu.add_command(label="Option2", command=lambda: print("Option2"))
+        self.label1.popupmenu.add_command(label="Option2", command=lambda: print("Option3"))
+        self.label1.grid(row=0, column=0)
+
+        # Main widget
+        self.mainwindow = self.frame1
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ExampleApp(root)
+    app.run()
--- core_apps/irontk/popup_menu.py
+++ core_apps/irontk/popup_menu.py
@@ -3,10 +3,10 @@ import tkinter as tk
 class PopupMenu(tk.Menu):
     def __init__(self, master):
         tk.Menu.__init__(self, master, tearoff=0, cursor="hand2")
-        master.bind("<Button-3>", lambda event: popup_right_click_menu(master, event)) # Button-2 on Aqua
+        master.bind("<Button-3>", lambda event: self.popup_right_click_menu(event)) # Button-2 on Aqua
 
-def popup_right_click_menu(master, event):
-        try:
-            master.popup_menu.tk_popup(event.x_root, event.y_root, 0)
-        finally:
-            master.popup_menu.grab_release()
+    def popup_right_click_menu(self, event):
+            try:
+                self.tk_popup(event.x_root, event.y_root, 0)
+            finally:
+                self.grab_release()

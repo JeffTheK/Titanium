3c44da7b331f4b237053806b8693e4683d810e99
JeffTheK <jeff144@protonmail.com>
2022-09-29 13:11:01 +0300

    project runner fix no space after args

--- core_apps/project_runner/__init__.py
+++ core_apps/project_runner/__init__.py
@@ -6,6 +6,6 @@ def run_project(path, args=None):
     project_json = json.loads(project_file.read())
     run_command = project_json["run_command"]
     if args != None:
-        run_command += args
+        run_command += " " + args
     run_command += " &"
     os.system(run_command)

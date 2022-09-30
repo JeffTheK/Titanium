72155aa587b9aa82e720f2aadc3c1b3f9a1a40f8
JeffTheK <jeff144@protonmail.com>
2022-09-30 16:46:11 +0300

    project creator add main.py

--- core_apps/project_creator/src/python.py
+++ core_apps/project_creator/src/python.py
@@ -1,16 +1,28 @@
 import os
+from tkinter.tix import MAIN
 
 PROJECT_JSON_FILE_TEMPLATE = """ {
     "run_command": "python3 -m core_apps.PROJECT_NAME.src.main"
 }
 """
 
+MAIN_PY_TEMPLATE = """#!/usr/bin/python3
+
+print("Hello World")
+"""
+
 def create_python_project(project_name, project_path):
     os.mkdir(project_path)
     os.mkdir(project_path / "src")
     create_project_json_file(project_name, project_path)
+    create_main_py(project_name, project_path)
 
 def create_project_json_file(project_name, project_path):
     project_json_file = open(os.path.join(project_path, ".project.json"), "w")
     project_json_file.write(PROJECT_JSON_FILE_TEMPLATE.replace("PROJECT_NAME", project_name))
+    project_json_file.close()
+
+def create_main_py(project_name, project_path):
+    project_json_file = open(os.path.join(project_path, "src/main.py"), "w")
+    project_json_file.write(MAIN_PY_TEMPLATE.replace("PROJECT_NAME", project_name))
     project_json_file.close()

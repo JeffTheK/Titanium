b0b92fa70926b4ea2d745acf87eda0b61690d532
JeffTheK <jeff144@protonmail.com>
2022-09-30 18:10:45 +0300

    finder add busy cursor while searching

--- core_apps/finder/src/main.py
+++ core_apps/finder/src/main.py
@@ -36,10 +36,13 @@ class Finder:
         search_location = self.search_path_entry.get()
         search_location = pathlib.Path(search_location)
         print(search_location)
+        self.mainwindow.config(cursor="watch")
+        self.mainwindow.update()
         found_items += search_location.rglob(f"*{search_term}*")
         self.clear_results_tree()
         for item in found_items:
             self.results_tree.insert("", tk.END, text=item, values=item)
+        self.mainwindow.config(cursor="")
 
 if __name__ == "__main__":
     app = Finder()

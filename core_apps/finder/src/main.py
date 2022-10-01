2b317e6557ab9339f96ffa5c8c5f1eddf2cb54ce
JeffTheK <jeff144@protonmail.com>
2022-10-01 11:51:30 +0300

    finder add icons

--- core_apps/finder/src/main.py
+++ core_apps/finder/src/main.py
@@ -5,6 +5,7 @@ import tkinter as tk
 import pygubu
 import glob
 from ... import tkinter_themes
+from tkfontawesome import icon_to_image
 
 PROJECT_PATH = pathlib.Path(__file__).parent
 PROJECT_UI = PROJECT_PATH / "main.ui"
@@ -20,6 +21,11 @@ class Finder:
         self.search_path_entry = builder.get_object("search_path_entry")
         self.search_path_entry.insert("0", SEARCH_LOCATION)
         self.results_tree = builder.get_object("results_tree", master)
+        self.file_icon = icon_to_image("file", scale_to_height=16, fill="#1D9F75")
+        self.folder_icon = icon_to_image("folder", scale_to_height=16, fill="#ebab34")
+        self.search_icon = icon_to_image("search", scale_to_height=16)
+        self.search_button = builder.get_object("search_button", master)
+        self.search_button.config(image=self.search_icon, compound="left")
         builder.connect_callbacks(self)
 
         tkinter_themes.setup_global_tkinter_theme(self.mainwindow)
@@ -41,7 +47,10 @@ class Finder:
         found_items += search_location.rglob(f"*{search_term}*")
         self.clear_results_tree()
         for item in found_items:
-            self.results_tree.insert("", tk.END, text=item, values=item)
+            if item.is_file():
+                self.results_tree.insert("", tk.END, text=" " + str(item), values=item, image=self.file_icon)
+            else:
+                self.results_tree.insert("", tk.END, text=" " + str(item), values=item, image=self.folder_icon)
         self.mainwindow.config(cursor="")
 
 if __name__ == "__main__":

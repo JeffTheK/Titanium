9c19320b781a902dc1e958929fa771665b30566c
JeffTheK <jeff144@protonmail.com>
2022-10-01 15:55:39 +0300

    code_actions_menu add do it icon

--- core_apps/code_actions_menu/menu.py
+++ core_apps/code_actions_menu/menu.py
@@ -6,16 +6,17 @@ from pathlib import Path
 from .. import code_runner
 from .. import irontk
 from .. import project_runner
+from tkfontawesome import icon_to_image
 
 class CodeActionsWidget:
     def __init__(self, master, edit_area_text):
         master.code_actions_widget = self
         self.language = "python"
+        self.do_it_icon = icon_to_image("play", scale_to_height=16, fill="#32a852")
         self.edit_area_text = edit_area_text
         self.edit_area_text.popup_menu = irontk.PopupMenu(self.edit_area_text)
-        self.do_it_image = tk.PhotoImage(file=Path( __file__ ).parent / "do_it.png")
-        self.edit_area_text.popup_menu.add_command(label=" Do It", command=self.on_do_it_pressed, image=self.do_it_image, compound="left")
-        self.edit_area_text.popup_menu.add_command(label=" Do It in New Window", command=self.do_it_in_new_window, image=self.do_it_image, compound="left")
+        self.edit_area_text.popup_menu.add_command(label=" Do It", command=self.on_do_it_pressed, image=self.do_it_icon, compound="left")
+        self.edit_area_text.popup_menu.add_command(label=" Do It in New Window", command=self.do_it_in_new_window, image=self.do_it_icon, compound="left")
 
     def on_do_it_pressed(self):
         code = self.edit_area_text.get(tk.SEL_FIRST, tk.SEL_LAST)

9a691fb5ee50dd1bd103a2dc3579c096384b6e3b
JeffTheK <jeff144@protonmail.com>
2022-10-26 12:19:28 +0300

    tutorial refactor

--- core_apps/tutorial/src/main.py
+++ core_apps/tutorial/src/main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import imp
 import pathlib
-import tkinter.ttk as ttk
 import tkinter as tk
 import pygubu
-import os
 from ... import tkinter_themes
 from tkfontawesome import icon_to_image
 
@@ -13,6 +10,7 @@ PROJECT_UI = PROJECT_PATH / "main.ui"
 PAGES_PATH = PROJECT_PATH.parent / "pages"
 
 class MainApp:
+    """An app with tutorial pages you can browse"""
     def __init__(self, master=None):
         self.builder = builder = pygubu.Builder()
         builder.add_resource_path(PROJECT_PATH)
@@ -22,6 +20,7 @@ class MainApp:
         self.page_tree.heading("#0", text="Pages")
         self.text = builder.get_object("text", master)
         self.page_icon = icon_to_image("file-alt", scale_to_height=16)
+        self.selected_page = None
         builder.connect_callbacks(self)
         self.setup_pages()
         children = self.page_tree.get_children()
@@ -32,18 +31,22 @@ class MainApp:
         tkinter_themes.setup_global_tkinter_theme(self.mainwindow)
 
     def run(self):
+        """Runs the app"""
         self.mainwindow.mainloop()
 
     def setup_pages(self):
+        """Puts pages into page tree from folder"""
         for page in PAGES_PATH.iterdir():
-            self.page_tree.insert('', tk.END, values=str(page.absolute()), text=" " + page.name, image=self.page_icon)
+            self.page_tree.insert('', tk.END, values=str(page.absolute()), text=" " + page.name,
+                image=self.page_icon)
         self.page_tree.focus_set()
 
-    def on_page_select(self, event=None):
+    def on_page_select(self, _event=None):
+        """When page is selected show text from the new page"""
         selected_item = self.page_tree.selection()[0]
         self.selected_page = self.page_tree.item(selected_item, "values")
         path = self.selected_page[0]
-        page_file = open(path, "r")
+        page_file = open(path, "r", encoding="utf8")
         self.text.delete('1.0', tk.END)
         self.text.insert('1.0', page_file.read())
         page_file.close()
